57358a3559e773cbcbaaa9d705e7dbe7696b046d
Dima636d <pupkinsk636@gmail.com>
2024-12-15 13:28:48 +0200

    added profile and profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,36 @@
 let username = "";
 let password = "";
+let money = 100; 
+let proms = { proms1: "QWERTY123456", proms2: "ZXCASD" };
 
-function load() {
+function start() {
     username = localStorage.getItem("username");
-    password = localStorage.getItem("password")
+    password = localStorage.getItem("password");
+    money = parseInt(localStorage.getItem("money")) || 100;
+    localStorage.setItem("money", money);
     document.getElementById('name').innerHTML = username ? username : "Гость"; 
-    document.getElementById('pass').innerHTML = password ? password : "пароль потерялся";
+    document.getElementById('pass').innerHTML = password + " пароль";
+    document.getElementById('moneyy').innerHTML = money ? money : "no money";
+    console.log("money print", money);
 }
+start();
 
-load();
+function addmoney() {
+    let enteredprom = document.getElementById('promInput').value.trim();
+
+    if (enteredprom === proms.proms1) {
+        money = parseInt(money) + 50;  
+        localStorage.setItem("money", money);  
+        money = parseInt(localStorage.getItem("money"));
+        document.getElementById('moneyy').innerHTML = money; 
+        console.log("money save", money);
+    }
+
+    if (enteredprom === proms.proms2) {
+        money = parseInt(money) + 142;  
+        localStorage.setItem("money", money);  
+        money = parseInt(localStorage.getItem("money"));
+        document.getElementById('moneyy').innerHTML = money; 
+        console.log("money save", money);
+    }
+}
