5fa09e447e550cd6664d1f3bb76090d7f32d0b47
Dima636d <pupkinsk636@gmail.com>
2024-12-19 12:38:38 +0200

    update

--- js/profile.js
+++ js/profile.js
@@ -3,11 +3,14 @@ let password = "";
 let money = 100; 
 let proms = { proms1: "QWERTY123456", proms2: "ZXCASD", proms3: "QRII1488" };
 
+const colorPicker = document.getElementById('colorPicker');
+
 function start() {
     username = localStorage.getItem("username");
     password = localStorage.getItem("password");
     money = parseInt(localStorage.getItem("money")) || 100;
     localStorage.setItem("money", money);
+    savedColor = localStorage.getItem('bgColor');
     let tokens = parseInt(localStorage.getItem("token"))
     document.getElementById('name').innerHTML = username ? username : "Гость"; 
     document.getElementById('pass').innerHTML = password + " пароль";
@@ -43,3 +46,9 @@ function addmoney() {
         console.log("money save", money);
     }
 }
+
+
+colorPicker.addEventListener('input', function() {
+    document.body.style.backgroundColor = colorPicker.value;
+    localStorage.setItem('bgColor', colorPicker.value);
+});
---
+++ js/shop.js
@@ -0,0 +1,8 @@
+function panel() {
+    const panel = document.getElementById('panel');
+    if (panel.style.display === "none" || panel.style.display === "") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+}
